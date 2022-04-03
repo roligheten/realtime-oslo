1576b036450e674c35e6c51020341c758b663c8c
Christian Barth Roligheten <christian@roligheten.no>
2022-04-03 23:20:09 +0200

    Fix deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env sh
-
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git checkout -b master
 git add -A
 git commit -m 'deploy'
 
